308533eca4ee588921adf11f2a393e3e46544ed6
Juhani Tapaninen <juhani.tapaninen@gofore.com>
2018-02-05 21:35:09 +0200

    Add some basic styles.

--- components/ShoppingList.js
+++ components/ShoppingList.js
@@ -1,25 +1,42 @@
 import {graphql} from 'react-apollo'
+import styled from 'styled-components';
+import {ButtonCircle, Text} from 'rebass';
 import gql from 'graphql-tag'
 
 const POSTS_PER_PAGE = 10
 
+const List = styled.ul `
+  margin: 2rem;
+`;
+
+const ListItem = styled.li `
+  padding: 1em 2em;
+  border-bottom: 1px solid ${props => props.theme.colors.gray};
+  transition: background-color 0.25s;
+  &:hover {
+    background-color: ${props => props.theme.colors.hover};
+  }
+`
+
 function ShoppingList({data: {loading,error,nextShoppingList}}) {;
   const {items} = nextShoppingList;
+  const handleClick = () => console.log('Click!');
+  console.log(items);
   if (error) 
     return <div>Error loading items.</div>
     if (items && items.length > 0) {
     return (
       <section>
-        <ul>
+        <List>
         {
           items.map(({item}) =>
-            <li key={item.id}>
-              <div>{item.name}</div>
-            </li>
+            <ListItem key={item.id} onClick={handleClick}>
+              <Text f={[ 4, 5 ]}>{item.name}</Text>
+            </ListItem>
           )}
-        </ul>
+        </List>
       </section>
-    )
+    );
   }
   return <div>Loading</div>
 }
